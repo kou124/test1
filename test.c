d787f18ec59a646db9d8a500afeb0d872d1e19a2
kou124 <2911484277@qq.com>
2022-12-17 16:07:18 +0800

    提交文件

---
+++ test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	//printf("hello world\n");
+//	//printf("%d\n",100);//打印整数-%d
+//	//sizeof-关键字-操作符-计算类型或者变量所占空间的大小
+//	printf("%u\n", sizeof(short));//sizeof打印用%u
+//	printf("%u\n", sizeof(char));
+//	printf("%u\n", sizeof(long));
+//	printf("%u\n", sizeof(long long));
+//	printf("%u\n", sizeof(int));
+//	printf("%u\n", sizeof(float));
+//	printf("%u\n", sizeof(double));
+//	return 0;
+//}
+//int main()
+//{
+//	int age = 20;
+//	double weight = 50.5;
+//	age = age + 1;
+//	weight = weight - 5;
+//	printf("%d\n", age);d打印整型数据
+//	printf("%lf\n", weight);lf打印double类型数据，f-float数据
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);scanf需要#define
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+//extern int g_val;全局变量测试
+//int main()
+//{
+//	printf("g_val=%d", g_val);
+//	return 0;
+//
+//}
+
+//int main()
+//{
+	//const int a = 10;
+    //printf("a=%d\n", a);//const 修饰常变量,具有常属性（不能改变的属性）
+	/*int arr[10] = {0};
+	const int n = 20;
+	int arr2[n] = {0};*/
+//#define max 10000//#define定义标识符常量
+//	int num = max;
+//	printf("num=%d\n", num);
+//枚举常量
+	/*enum sex
+	{
+		male=3,
+		flmale,
+		secret
+	};
+	enum sex s = male;
+	printf("%d\n", male);
+	printf("%d\n", flmale);
+	printf("%d\n", secret);*/
+	//return 0;
+//}
+//#include <string.h>
+//int main()
+//{  
+//	char arr1[] = "kou";
+//	char arr2[] = { 'k','o','u','\0' };
+//	printf("%s\n", arr1);//打印字符串%s
+//	printf("%s\n", arr2);
+//	//int len = strlen("kou");strlen:字符串长度
+//	//printf("%d\n", len);
+//	printf("%u\n", strlen(arr1));//打印字符串长度；strlen打印用%u
+//	printf("%u\n", strlen(arr2));
+//	return 0;
+//}
